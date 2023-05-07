969e124ee86a885e2944bb22cf8e52b5e4a104cf
Dursun <93613110+Dursunipek@users.noreply.github.com>
2023-05-07 18:51:38 +0300

    Add tourismAgencyDbContext

---
+++ Tourism-Agency-AspNet-Web-Api/Tourism-Agency-AspNet-Web-Api/Data/TourismAgencyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tourism_Agency_AspNet_Web_Api.Models;
+
+namespace Tourism_Agency_AspNet_Web_Api.Data
+{
+    public class TourismAgencyDbContext : DbContext
+    {
+        public TourismAgencyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
