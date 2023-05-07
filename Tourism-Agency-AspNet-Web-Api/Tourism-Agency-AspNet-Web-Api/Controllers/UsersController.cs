c00b932a8cdcab966dcd6a62f367838354f6e219
Dursun <93613110+Dursunipek@users.noreply.github.com>
2023-05-07 19:14:44 +0300

    Add test user controller

---
+++ Tourism-Agency-AspNet-Web-Api/Tourism-Agency-AspNet-Web-Api/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tourism_Agency_AspNet_Web_Api.Data;
+
+namespace Tourism_Agency_AspNet_Web_Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly TourismAgencyDbContext _tourismAgencyDbContext;
+
+        public UsersController(TourismAgencyDbContext tourismAgencyDbContext)
+        {
+            _tourismAgencyDbContext = tourismAgencyDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _tourismAgencyDbContext.Users.ToListAsync();
+            return Ok(users);
+        }
+    }
+}
